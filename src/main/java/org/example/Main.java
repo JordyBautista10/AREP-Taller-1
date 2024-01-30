7d471738fa6ea95d45cfa3962bfb3288a01937b5
JordyBautista10 <123812969+JordyBautista10@users.noreply.github.com>
2024-01-29 19:10:36 -0500

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,162 @@
+package org.example;
+
+import java.net.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        boolean running = true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            boolean firstLine = true;
+            String uriStr ="";
+
+            while ((inputLine = in.readLine()) != null) {
+                if(firstLine){
+                    uriStr = inputLine.split(" ")[1];
+                    firstLine = false;
+                }
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            if(uriStr.startsWith("/Cliente")){
+                outputLine = httpClientHtml();
+            }else if (uriStr.startsWith("/Busqueda")){
+                outputLine = makeRequest("http://www.omdbapi.com/?apikey=b1060e61&t=" + uriStr.split("=")[1]);
+                System.out.println("-----------------------: " + uriStr + outputLine);
+            }
+            else {
+                outputLine = httpError();
+            }
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+     public static String makeRequest(String url) throws IOException {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        StringBuffer response = new StringBuffer();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+        return response.toString();
+    }
+
+    public static String httpError() {
+        return "HTTP/1.1 400 Not found\r\n" //encabezado necesario
+                + "Content-Type:text/html\r\n"
+                + "\r\n" //retorno de carro y salto de linea
+                + "<!DOCTYPE html>\n" +
+                "<html lang=\"es\">\n" +
+                "<head>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "</body>\n" +
+                "</html>\n";
+
+    }
+
+    public static String httpClientHtml() {
+        return "HTTP/1.1 200 OK\r\n" //encabezado necesario
+                + "Content-Type:text/html\r\n"
+                + "\r\n" //retorno de carro y salto de linea
+                + "<!DOCTYPE html>"
+                + "<html>\n"
+                + "    <head>\n"
+                + "        <title>Form Example</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        <h1>Form with GET</h1>\n"
+                + "        <form action=\"/hello\">\n"
+                + "            <label for=\"name\">Name:</label><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n"
+                + "        </form> \n"
+                + "        <div id=\"getrespmsg\"></div>\n"
+                + "\n"
+                + "        <script>\n"
+                + "            function loadGetMsg() {\n"
+                + "                let nameVar = document.getElementById(\"name\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                }\n"
+                + "                xhttp.open(\"GET\", \"/Busqueda?t=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "        </script>\n"
+                + "\n"
+                + "        <h1>Form with POST</h1>\n"
+                + "        <form action=\"/hellopost\">\n"
+                + "            <label for=\"postname\">Name:</label><br>\n"
+                + "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n"
+                + "        </form>\n"
+                + "        \n"
+                + "        <div id=\"postrespmsg\"></div>\n"
+                + "        \n"
+                + "        <script>\n"
+                + "            function loadPostMsg(name){\n"
+                + "                let url = \"/hellopost?name=\" + name.value;\n"
+                + "\n"
+                + "                fetch (url, {method: 'POST'})\n"
+                + "                    .then(x => x.text())\n"
+                + "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n"
+                + "            }\n"
+                + "        </script>\n"
+                + "    </body>\n"
+                + "</html>";
+
+
+    }
+}

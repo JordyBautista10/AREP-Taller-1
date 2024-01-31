e73f6a60483721d85a49353d9d78e387ccf5d687
JordyBautista10 <jordy.sbautista@gmail.com>
2024-01-31 18:59:22 -0500

    Conexion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,13 @@ package org.example;
 
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
+        HashMap<String, String> cache = new HashMap<String, String>();
+
         try {
             serverSocket = new ServerSocket(35000);
         } catch (IOException e) {
@@ -44,8 +47,17 @@ public class Main {
             }
             if(uriStr.startsWith("/Cliente")){
                 outputLine = httpClientHtml();
-            }else if (uriStr.startsWith("/Busqueda")){
-                outputLine = makeRequest("http://www.omdbapi.com/?apikey=b1060e61&t=" + uriStr.split("=")[1]);
+            }else if (uriStr.startsWith("/Busqueda") && uriStr.length() > 12){
+                String nameMovie = uriStr.substring(12).toLowerCase();
+                if (!cache.containsKey(nameMovie)){
+                    System.out.println("No se encontro en el cache");
+                    outputLine = makeRequest("http://www.omdbapi.com/?apikey=b1060e61&t=" + nameMovie);
+                    cache.put(nameMovie, outputLine);
+                } else {
+                    System.out.println("Se encontro en el cache Bien");
+                    outputLine = cache.get(nameMovie);
+                }
+                //Cache
                 System.out.println("-----------------------: " + uriStr + outputLine);
             }
             else {
@@ -75,6 +87,7 @@ public class Main {
                     con.getInputStream()));
             String inputLine;
 
+            response.append("HTTP/1.1 200 OK\r\n" + "Content-Type:application/json\r\n" + "\r\n"); // Encabezado
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
@@ -86,6 +99,7 @@ public class Main {
             System.out.println("GET request not worked");
         }
         System.out.println("GET DONE");
+
         return response.toString();
     }
 
@@ -100,7 +114,6 @@ public class Main {
                 "<body>\n" +
                 "</body>\n" +
                 "</html>\n";
-
     }
 
     public static String httpClientHtml() {
@@ -116,9 +129,9 @@ public class Main {
                 + "    </head>\n"
                 + "    <body>\n"
                 + "        <h1>Form with GET</h1>\n"
-                + "        <form action=\"/hello\">\n"
+                + "        <form action=\"/Busqueda\">\n"
                 + "            <label for=\"name\">Name:</label><br>\n"
-                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"t\" value=\"John\"><br><br>\n"
                 + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n"
                 + "        </form> \n"
                 + "        <div id=\"getrespmsg\"></div>\n"
@@ -128,8 +141,11 @@ public class Main {
                 + "                let nameVar = document.getElementById(\"name\").value;\n"
                 + "                const xhttp = new XMLHttpRequest();\n"
                 + "                xhttp.onload = function() {\n"
-                + "                    document.getElementById(\"getrespmsg\").innerHTML =\n"
-                + "                    this.responseText;\n"
+                + "                    var jsonResponse = JSON.parse(this.responseText);"
+                + "                    var htmlContent = \"<b>Title: </b>\" + jsonResponse.Title + \"<br>\";"
+                + "                    htmlContent += \"<b> Year: </b>\" + jsonResponse.Year + \"<br>\";"
+                + "                    htmlContent += \"<b> RatedRated: </b>\" + jsonResponse.Rated + \"<br>\";"
+                + "                    document.getElementById(\"getrespmsg\").innerHTML = htmlContent \n"
                 + "                }\n"
                 + "                xhttp.open(\"GET\", \"/Busqueda?t=\"+nameVar);\n"
                 + "                xhttp.send();\n"
@@ -156,7 +172,6 @@ public class Main {
                 + "        </script>\n"
                 + "    </body>\n"
                 + "</html>";
-
-
     }
+
 }
